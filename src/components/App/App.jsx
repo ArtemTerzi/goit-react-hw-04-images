35313a063bcdf9ade365d8b2aae780828334d360
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-04-10 11:46:20 +0300

    ver 1.0 Release

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
 import sass from './App.module.scss';
+import { fetchData } from 'helpers/fetchAPI';
 
 const App = () => {
   const [query, setQuery] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = useCallback(() => {
+    fetchData(query, 1).then(response => {
+      if (!response.hits.length) {
+        setStatus('rejected');
+        return;
+      }
+      setImages(response.hits);
+      setStatus('resolved');
+      setPage(1);
+      if (response.totalHits === response.hits.length) {
+        setStatus('idle');
+      }
+    });
+  }, [query]);
+
+  useEffect(() => {
+    try {
+      if (query && page === 1) {
+        getData();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [getData, page, query]);
+
+  const onLoadMore = () => {
+    setStatus('pending');
+    setPage(prev => prev + 1);
+
+    fetchData(query, page + 1).then(response => {
+      setImages(prev => [...prev, ...response.hits]);
+      setStatus('resolved');
+    });
+  };
 
   const handleSubmit = query => {
+    setImages([]);
+    setPage(1);
     setQuery(query);
   };
 
   return (
     <div className={sass.app}>
       <Searchbar onSubmit={handleSubmit} />
-      <ImageGallery query={query} />
+      <ImageGallery
+        query={query}
+        images={images}
+        status={status}
+        onLoadMore={onLoadMore}
+      />
     </div>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,67 +1,15 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import sass from './ImageGallery.module.scss';
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import { fetchData } from 'helpers/fetchAPI';
 import Button from 'components/Button';
 import Loader from 'components/Loader';
 import Modal from 'components/Modal';
 import PropTypes from 'prop-types';
 import Error from 'components/Error';
 
-const ImageGallery = ({ query }) => {
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [status, setStatus] = useState('idle');
+const ImageGallery = ({ status, images, onLoadMore }) => {
   const [isModalOpen, setisModalOpen] = useState(false);
   const [modalIMG, setModalIMG] = useState('');
-  const isFisrtRender = useRef(true);
-  const prevQuery = useRef('');
-
-  const getData = useCallback(() => {
-    fetchData(query, 1).then(response => {
-      if (!response.hits.length) {
-        setStatus('rejected');
-        prevQuery.current = query;
-        return;
-      }
-      setImages(response.hits);
-      setStatus('resolved');
-      setPage(1);
-      if (response.totalHits === response.hits.length) {
-        setStatus('idle');
-      }
-      prevQuery.current = query;
-    });
-  }, [query]);
-
-  useEffect(() => {
-    if (isFisrtRender.current) {
-      isFisrtRender.current = false;
-      return;
-    }
-
-    try {
-      if (prevQuery.current !== query && page !== 1) {
-        getData();
-      }
-
-      if (query.length >= 1 && page === 1 && prevQuery.current !== query) {
-        getData();
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }, [getData, page, query]);
-
-  const onLoadMore = () => {
-    setStatus('pending');
-    setPage(prev => prev + 1);
-
-    fetchData(query, page + 1).then(response => {
-      setImages(prev => [...prev, ...response.hits]);
-      setStatus('resolved');
-    });
-  };
 
   const showModal = image => {
     setisModalOpen(true);
